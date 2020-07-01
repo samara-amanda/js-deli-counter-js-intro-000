59571ea2a023469f87f05fa890b282ac3ffc4e66
Samara Grant <samara.amanda01@gmail.com>
2020-07-01 21:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(deliLine) {
       return "There is nobody waiting to be served!"
     } else {
       return deliLine.pop()
-      return `Currently serving ${deliLine[0]}.`
+      return `Currently serving ${deliLine}.`
 
     }
 }
